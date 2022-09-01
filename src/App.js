33d1a33e9b042bd726092c3ac8a1447b63d6aef1
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-01 09:53:50 +0600

    router setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route></Route>
+        <Route path='/' element={<Home></Home>} ></Route>
       </Routes>
 
     </div>
---
+++ src/components/SignUp/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUp = () => {
+    return (
+        <div>
+            <h2>sing up</h2>
+        </div>
+    );
+};
+
+export default SignUp;
